e4c0308d7008f3c224d50c0aacfdc844166e3ba0
Jonas <jonas.nussdorfer@gmail.com>
2023-03-19 08:03:30 +0000

    save state before updating conan in container

--- apps/mct-solve/src/main.cpp
+++ apps/mct-solve/src/main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <concepts>
+
+template <typename T>
+concept ReturnPolicy = requires{ typename T::Type; };
+
+template <class T, template<typename> class RETURN_POLICY>
+struct DataTransformer
+{
+    using ReturnType = RETURN_POLICY<T>::Type;
+
+    ReturnType calculateStep(const T& previous) { return {};}
+
+    
+};
 
 int main()
 {
